651b7061b97089a22a6d33a472ccd45cafc191d3
copycd@daum.net <copycd@daum.net>
2022-06-23 19:11:27 +0900

    + 최신 버전을 가져와서, 기존에 가지고 있던것 일부 갱신함.

---
+++ DelaunatorSharp/Models/CmPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelaunatorSharp.Models
+{
+   /// <summary>
+   /// 기존 Point에 고도값이나, 기타 정보를 포함하기 위해서.
+   /// </summary>
+	public struct CmPoint : IPoint
+   {
+      public double X { get; set; }
+      public double Y { get; set; }
+      public object tag { get; set; }
+
+      public CmPoint(double x, double y, object userTag = null )
+      {
+         X = x;
+         Y = y;
+         this.tag = userTag;
+      }
+      public override string ToString() => $"{X},{Y}";
+   }
+}
