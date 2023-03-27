58e89d6b658166d630455895b8251ccc52ff2906
copycd <copycd@daum.net>
2023-03-27 11:30:12 +0900

    + nuget update.

--- DelaunatorSharp/Models/CCdPoint.cs
+++ DelaunatorSharp/Models/CCdPoint.cs
@@ -1,24 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DelaunatorSharp.Models
+﻿namespace DelaunatorSharp.Models
 {
-   /// <summary>
-   /// 기존 Point에 고도값이나, 기타 정보를 포함하기 위해서.
-   /// </summary>
-	public struct CCdPoint : IPoint
-   {
-      public double X { get; set; }
-      public double Y { get; set; }
-      public object tag { get; set; }
+    /// <summary>
+    /// 기존 Point에 고도값이나, 기타 정보를 포함하기 위해서.
+    /// </summary>
+    public struct CCdPoint : IPoint
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public object tag { get; set; }
 
-      public CCdPoint(double x, double y, object userTag = null )
-      {
-         X = x;
-         Y = y;
-         this.tag = userTag;
-      }
-      public override string ToString() => $"{X},{Y}";
-   }
+        public CCdPoint(double x, double y, object userTag = null)
+        {
+            X = x;
+            Y = y;
+            this.tag = userTag;
+        }
+        public override string ToString() => $"{X},{Y}";
+    }
 }
